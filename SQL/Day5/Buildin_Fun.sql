bef75a6ee0e53024fe731df1986854ea5d428877
lokeshj <lokeshj022004@gmail.com>
2026-07-31 20:44:49 +0530

    updated

---
+++ SQL/Day5/Buildin_Fun.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE CompanyDB;
+
+-- Use Database
+USE CompanyDB;
+
+-- Create Employees Table
+CREATE TABLE Employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    city VARCHAR(50)
+);
+
+-- Insert 20 Employee Records
+INSERT INTO Employees (id, name, department, salary, city) VALUES
+(1, 'Arun', 'HR', 35000, 'Chennai'),
+(2, 'Priya', 'IT', 60000, 'Coimbatore'),
+(3, 'Karthik', 'Finance', 55000, 'Madurai'),
+(4, 'Divya', 'Marketing', 45000, 'Salem'),
+(5, 'Rahul', 'IT', 70000, 'Chennai'),
+(6, 'Sneha', 'HR', 40000, 'Trichy'),
+(7, 'Vignesh', 'Sales', 38000, 'Erode'),
+(8, 'Meena', 'Finance', 62000, 'Coimbatore'),
+(9, 'Suresh', 'Marketing', 50000, 'Madurai'),
+(10, 'Anitha', 'IT', 75000, 'Chennai'),
+(11, 'Ravi', 'Sales', 42000, 'Salem'),
+(12, 'Keerthi', 'HR', 39000, 'Trichy'),
+(13, 'Manoj', 'Finance', 68000, 'Erode'),
+(14, 'Pooja', 'Marketing', 47000, 'Coimbatore'),
+(15, 'Ajith', 'IT', 80000, 'Chennai'),
+(16, 'Nisha', 'Sales', 41000, 'Madurai'),
+(17, 'Harish', 'HR', 36000, 'Salem'),
+(18, 'Deepa', 'Finance', 65000, 'Trichy'),
+(19, 'Kiran', 'Marketing', 52000, 'Erode'),
+(20, 'Lavanya', 'IT', 85000, 'Coimbatore');
+
+-- Display all records
+SELECT * FROM Employees;
+-- task 1
+select department,count(*)"Total Employees",sum(salary)"Total salary",avg(salary)"Average salary",max(salary)"Highest salary",min(salary)"Lowest salary" from Employees group by department;
+-- task 2
+select city,count(*)"Total Employees",max(salary)"Highest salary",min(salary)"Lowest salary",avg(salary)"Average salary" from Employees  group  by city order by max(salary)DESC;
+-- task 3
+select department ,count(*)"Total Employees",sum(salary)from Employees group by department order by max(salary) DESC;  
+-- task 4
+SELECT department , city ,count(*),avg(salary) from Employees group by department, city;
+-- task 5
+SELECT department , count(*),sum(salary),avg(salary),max(salary),min(salary) from Employees group by department ORDER BY sum(salary) DESC LIMIT 3;
